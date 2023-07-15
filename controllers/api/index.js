c524835325f67e308270a33785aa3a1bc6da059b
Kevin Lewis <20131699+KPL33@users.noreply.github.com>
2023-07-14 17:51:19 -0700

    Routes bundled properly

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,1 +1,10 @@
-//For our "Routes"
+const router = require('express').Router();
+const loginRoutes = require('./loginRoutes');
+const postRoutes = require('./postRoutes');
+const signupRoutes = require('./signupRoutes');
+
+router.use('/loginRoutes', loginRoutes);
+router.use('/postRoutes', postRoutes);
+router.use('/signupRoutes', signupRoutes);
+
+module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -1,18 +1,12 @@
 //"Controllers" folder is here to hold all of "Routes". This "index" file handles the logic between the "Views" and the "Models". 
 const router = require('express').Router();
 //Dane had you comment out the api stuff here. Needed later?
-// const apiRoutes = require('./api');
+const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
 
-const postRoutes = require('./api/postRoutes');
-
-const loginRoutes = require('./api/loginRoutes');
 
 router.use('/', homeRoutes);
-// router.use('/api', apiRoutes);
+router.use('/api', apiRoutes);
 
-router.use('/api', postRoutes);
-
-router.use(loginRoutes);
 
 module.exports = router;
