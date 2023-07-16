58bccd6288195e3616bb82e8535af4c4f0587ca8
Kevin Lewis <20131699+KPL33@users.noreply.github.com>
2023-07-16 14:25:54 -0700

    Minor changes made to homeRoutes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -7,7 +7,7 @@ router.get('/', async (req, res) => {
   try {
     const postData = await Post.findAll();
     const posts = postData.map((post) => post.get({ plain: true }));
-    res.render('homepage', { posts, layout: 'main' });
+    res.render('homepage', { ...posts, layout: 'main' });
   } catch (err) {
     res.status(500).json(err);
   }
--- controllers/index.js
+++ controllers/index.js
@@ -5,8 +5,8 @@ const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
 
 
-router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
+router.use('/', homeRoutes);
 
 
 module.exports = router;
