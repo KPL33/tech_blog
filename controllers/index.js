5ba22e41c2c81771704f67fcec1bba43b898aed4
Kevin Lewis <20131699+KPL33@users.noreply.github.com>
2023-07-17 21:50:11 -0700

    Note cleanup

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,5 @@
 //"Controllers" folder is here to hold all of "Routes". This "index" file handles the logic between the "Views" and the "Models". 
 const router = require('express').Router();
-//Dane had you comment out the api stuff here. Needed later?
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
 
